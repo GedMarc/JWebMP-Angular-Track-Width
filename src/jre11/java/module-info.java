e148f9578d29c42929490914dc40046c28ad6539
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:34:54 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,12 +3,12 @@ module com.jwebmp.plugins.angulartrackwidth {
 
 	requires com.jwebmp.core;
 	requires java.validation;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angulartrackwidth.AngularTrackWidthPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularDirective with com.jwebmp.plugins.angulartrackwidth.AngularTrackWidthDirective;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angulartrackwidth.implementations.AngularTrackWidthExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angulartrackwidth.implementations.AngularTrackWidthExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angulartrackwidth.implementations.AngularTrackWidthExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angulartrackwidth.implementations.AngularTrackWidthExclusionsModule;
 }
