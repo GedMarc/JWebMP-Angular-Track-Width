af03d8b949c3bc559daec204392312e5f4ad3e58
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:38:43 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.plugins.angulartrackwidth {
 	exports com.jwebmp.plugins.angulartrackwidth;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
