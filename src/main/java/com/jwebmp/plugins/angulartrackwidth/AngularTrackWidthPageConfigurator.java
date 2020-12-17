cc6c19038de883fa4ee2595080df94b597624194
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:19 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/angulartrackwidth/AngularTrackWidthPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angulartrackwidth/AngularTrackWidthPageConfigurator.java
@@ -91,7 +91,7 @@ public class AngularTrackWidthPageConfigurator
 
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
